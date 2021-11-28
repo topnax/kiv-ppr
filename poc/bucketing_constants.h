65813901c7d32eadf0624f62247921a1342d618d
Stanislav Král <stanislav.kral3@gmail.com>
2021-11-28 11:11:23 +0100

    Fixed MAX_BUCKET_COUNT constraint as well as replaced "unsigned long" constants with uint64_t.

--- poc/bucketing_constants.h
+++ poc/bucketing_constants.h
@@ -30,26 +30,25 @@ const int NUMBER_SHIFT = NUMBER_SIZE_BITS - BUCKET_BITS;
 const int BUCKET_KEY_LENGTH_BITS = NUMBER_SIZE_BITS - NUMBER_SHIFT;
 
 // calculate the number of buckets
-const unsigned long BUCKET_COUNT = pow(2, BUCKET_KEY_LENGTH_BITS);
+const uint64_t BUCKET_COUNT = pow(2, BUCKET_KEY_LENGTH_BITS);
 
 // number of bits to be taken from mantissa when performing subbucketing
-const unsigned long SUB_BUCKET_MANTISSA_BITS = 20;
+const uint64_t SUB_BUCKET_MANTISSA_BITS = 20;
 
 // number of sub buckets
-const unsigned long SUB_BUCKET_COUNT = pow(2, SUB_BUCKET_MANTISSA_BITS);
+const uint64_t SUB_BUCKET_COUNT = pow(2, SUB_BUCKET_MANTISSA_BITS);
 
 // subbucket mantissa bit mask :)
-const unsigned long SUB_BUCKET_MANTISSA_MASK =
+const uint64_t SUB_BUCKET_MANTISSA_MASK =
         ((NUMBER_MAX >> (NUMBER_SIZE_BITS - (BUCKET_BITS + SUB_BUCKET_MANTISSA_BITS)))
                 << (NUMBER_SIZE_BITS - SUB_BUCKET_MANTISSA_BITS)) >> BUCKET_BITS;
 
 // number of bits to shift the loaded number by when calculating subbucket index
-const unsigned long SUB_BUCKET_SHIFT = NUMBER_SIZE_BITS - (SUB_BUCKET_MANTISSA_BITS + BUCKET_BITS);
+const uint64_t SUB_BUCKET_SHIFT = NUMBER_SIZE_BITS - (SUB_BUCKET_MANTISSA_BITS + BUCKET_BITS);
 
 
 // CONSTRAINTS:
-const unsigned long MEBI_BYTE = pow(pow(2, 10), 2);
-const unsigned long MEMORY_LIMIT = MEBI_BYTE * 250;
+const uint64_t MEBI_BYTE = pow(pow(2, 10), 2);
+const uint64_t MEMORY_LIMIT = MEBI_BYTE * 250;
 
-// TODO incorrect bucket count
-const unsigned long MAX_BUCKET_COUNT = (unsigned long) ((double) MEMORY_LIMIT * 0.8);
+const uint64_t MAX_BUCKET_COUNT = (unsigned long) (((double) MEMORY_LIMIT * 0.8) / NUMBER_SIZE_BYTES);
