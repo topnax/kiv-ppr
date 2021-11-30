78285b235c0f921b2601db1780ff4daa7db36354
Stanislav Král <stanislav.kral3@gmail.com>
2021-11-30 15:03:39 +0100

    Added comments to opencl_utils files.

--- src/opencl_utils.h
+++ src/opencl_utils.h
@@ -12,6 +12,24 @@
 // local include instead of the global one (so that the teacher can run it without installing additional header files)
 #include "opencl.hpp"
 
+/**
+ * Make an attempt to find an OpenCL device by the given name.
+ *
+ * @param device_name name of the device to be found
+ * @param out_device device reference to be written to if the device is find
+ *
+ * @return `true` when device was found, `false` otherwise
+ */
 bool cl_get_device(const std::string &device_name, cl::Device &out_device);
 
+/**
+ * Compile an OpenCL program
+ *
+ * @param program_content program itself
+ * @param program_name name of the program to be run
+ * @param context OpenCL context
+ * @param dev OpenCL device to be compiled for
+ *
+ * @return the compile OpenCl program
+ */
 cl::Program get_program(const std::string &program_content, const std::string &program_name, cl::Context &context, cl::Device &dev);
