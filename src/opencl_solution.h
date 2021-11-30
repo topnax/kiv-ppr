eaa34a6f35971ecf96a0f51ebb9d73046d154d2e
Stanislav Král <stanislav.kral3@gmail.com>
2021-11-30 15:00:47 +0100

    Added comments to opencl_solution files.

--- src/opencl_solution.h
+++ src/opencl_solution.h
@@ -5,4 +5,12 @@
 
 #include "solution.h"
 
+/**
+ * Process the file using an OpenCL device for a solution
+ * @param file_name file to be processed
+ * @param percentile  percentile to be found
+ * @param device OpenCL device to be used
+ *
+ * @return solution result
+ */
 solution_result process_file_opencl(char *file_name, int percentile, cl::Device &device);
