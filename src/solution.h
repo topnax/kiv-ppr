f37a336dbaf0676275c9848d71a0eadcb1d8b408
Stanislav Král <stanislav.kral3@gmail.com>
2021-11-30 15:35:42 +0100

    Added comments to solution.h files.

--- src/solution.h
+++ src/solution.h
@@ -7,8 +7,19 @@
 #include <cstdint>
 #include "params.h"
 
+// solution pair result alias
 using solution_result = std::pair<double, std::pair<uint64_t, uint64_t>>;
 
+/**
+ * Process the given parameters for a solution
+ * @param params parameters of the processing
+ * @return a solution result
+ */
 solution_result process_for_solution(parameters params);
 
+/**
+ * Print the solution result
+ *
+ * @param result result to be printed
+ */
 void print_solution_result(solution_result result);
