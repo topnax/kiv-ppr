8f7f61ab41ec5007a117c15ef1484edb8e40a111
Stanislav Král <stanislav.kral3@gmail.com>
2021-11-30 15:27:36 +0100

    Added comments to smp_solution files.

--- src/smp_solution.cpp
+++ src/smp_solution.cpp
@@ -9,10 +9,13 @@
 
 
 solution_result process_file_smp(char *file_name, int percentile) {
+    // create buckets
     auto buckets_and_item_count = create_buckets_smp(file_name);
 
+    // find the percentile position
     auto position = find_percentile_position(percentile, buckets_and_item_count.first, buckets_and_item_count.second);
 
+    // find the value of the percentile
     auto result = find_percentile_value_smp(
             position.second,
             position.first, file_name
--- src/smp_solution.h
+++ src/smp_solution.h
@@ -5,4 +5,11 @@
 
 #include "bucketing_utils.h"
 
+/**
+ * Process the file using multiple threads
+ * @param file_name file to be processed
+ * @param percentile  percentile to be found
+ *
+ * @return solution result
+ */
 solution_result process_file_smp(char *file_name, int percentile);
