e2de98b17d1bdf22b80d5040fe51e70abc701390
Stanislav Král <stanislav.kral@smart-software.cz>
2021-12-02 23:46:26 +0100

    Disabled optimizations for Intel device as they yield incorrect results. (thanks to @Spiffyk)

--- src/opencl/opencl_utils.cpp
+++ src/opencl/opencl_utils.cpp
@@ -5,6 +5,17 @@
 #include "opencl_utils.h"
 #include <algorithm>
 
+/**
+ * Checks whether the given device is an Intel device or not (thanks to @Spiffyk for this function)
+ * @param dev device to be checked
+ * @return boolean flag indicating whether the given device is an Intel device or not
+ */
+bool is_intel(cl::Device dev) {
+    std::string vendor(dev.getInfo<CL_DEVICE_VENDOR>());
+    std::transform(vendor.begin(), vendor.end(), vendor.begin(), ::tolower);
+    return vendor.find("intel") != std::string::npos;
+}
+
 bool cl_get_device(const std::string &device_name, cl::Device &out_device) {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
@@ -36,7 +47,14 @@ cl::Program get_program(const std::string &program_content, const std::string &p
                         cl::Device &dev) {
     cl::Program program(context, program_content);
 
-    auto error = program.build("-cl-std=CL2.0");
+    std::string build_flags = "-cl-std=CL2.0";
+    // optimizations are disabled for Intel, because their chips yield incorrect results otherwise and I don't
+    // own any device with Intel HD GPU, so I am not able to debug it
+    if (is_intel(dev)) {
+        build_flags += " -cl-opt-disable";
+    }
+
+    auto error = program.build(build_flags.c_str());
     if (error != CL_BUILD_SUCCESS) {
         std::string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev);
         printf("Build log: %s\n", buildlog.c_str());
